8fe54cd5a23de86a35b300427830066d910aa340
seungah <fofo2525@naver.com>
2017-06-13 20:06:50 +0900

    #2 MyDBHelper class

--- app/src/main/java/starbloom0128/kr/hs/emirim/dbex/MainActivity.java
+++ app/src/main/java/starbloom0128/kr/hs/emirim/dbex/MainActivity.java
@@ -1,5 +1,8 @@
 package starbloom0128.kr.hs.emirim.dbex;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    class MyDBHelper extends SQLiteOpenHelper{//추상 클래스
+        // idolDB라는 이름의 데이터베이스가 생성된다.
+
+        public MyDBHelper(Context context) {
+            super(context, "idolDB", null, 1);
+        }
+        //idolTable이라는 이름의 테이블 생성
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+            String sql="create table idolTable(idolname text not null primary key, idolCount integer)";
+            sqLiteDatabase.execSQL(sql);
+        }
+        //이미 idolTable이 존재한다면 기존의 테이블을 삭제하고 새로 테이블 만들 때 호출
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+            String sql="drop table if exist idolTable";
+            sqLiteDatabase.execSQL(sql);
+            onCreate(sqLiteDatabase);
+        }
     }
 }
